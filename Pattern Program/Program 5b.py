3a1b53bd13a110c086bdbcd9518b336451acfa27
Pratiksha Choudhary <choudharypratiksha36@gmail.com>
2026-02-06 00:29:45 +0530

    Pattern Program 5

---
+++ Pattern Program/Program 5b.py
@@ -0,0 +1,12 @@
+#Pattern Program
+
+'''
+*
+* *
+* * *
+* * * *
+* * * * *
+'''
+
+for i in range(1, 6):
+    print("* " * i)
---
+++ Pattern Program/Program 5c.py
@@ -0,0 +1,13 @@
+#Pattern Program 
+
+'''
+1 1 1 1 1
+2 2 2 2
+3 3 3
+4 4
+5
+'''
+
+n = 5
+for i in range(1, n+1):
+    print((str(i) + " ") * (n-i+1))
