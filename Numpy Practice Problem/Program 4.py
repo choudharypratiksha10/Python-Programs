7e999be0b05fdaf3cb6d2ab9da54146bba957e37
Pratiksha Choudhary <choudharypratiksha36@gmail.com>
2026-02-06 08:33:01 +0530

    Program 4
    Set Difference Between Two Arrays
    
    Write a NumPy program to find the set difference between two arrays.
    The set difference will return sorted, distinct values in array1 that are not in array2.
    
    Expected Output:
    Array1: [0, 10, 20, 40, 60, 80]
    Array2: [10, 30, 40, 50, 70, 90]
    Set difference between two arrays: [0, 20, 60, 80]

---
+++ Numpy Practice Problem/Program 4.py
@@ -0,0 +1,15 @@
+#Set difference between two arrays
+
+import numpy as np
+
+array1 = np.array([0, 10, 20, 40, 60, 80])
+array2 = np.array([10, 30, 40, 50, 70, 90])
+
+result = np.setdiff1d(array1, array2)
+
+print("Array1:", array1)
+print("Array2:", array2)
+print()
+
+print("Set difference between two arrays:")
+print(result)
