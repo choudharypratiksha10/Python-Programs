0c2de6655069f0d4fa4f07ce80b634cdcea867f8
Pratiksha Choudhary <choudharypratiksha36@gmail.com>
2026-02-24 20:22:32 +0530

    Program 17
    17. Write a Python program to find the common elements between two lists.

---
+++ String_List/List-Programs/Program 17.py
@@ -0,0 +1,7 @@
+#17. Find common elements between two lists
+
+list1 = list(map(int, input("Enter first list: ").split()))
+list2 = list(map(int, input("Enter second list: ").split()))
+
+common = list(set(list1) & set(list2))
+print("Common elements:", common)
