ea0e11309da21bef4898a2e4f7823d6253861ffe
Pratiksha Choudhary <choudharypratiksha36@gmail.com>
2026-02-24 18:54:24 +0530

    Program 41
    41. Python program to find the least frequent character in the string.

---
+++ String_List/String-Programs/Program 41.py
@@ -0,0 +1,10 @@
+#41. Least frequent character in a string
+
+s = input("Enter string: ")
+
+freq = {}
+for ch in s:
+    freq[ch] = freq.get(ch, 0) + 1
+
+least = min(freq, key=freq.get)
+print("Least frequent character:", least)
