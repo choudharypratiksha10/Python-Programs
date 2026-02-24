5d244e4389782128ce5481603ecedd72af0d4255
Pratiksha Choudhary <choudharypratiksha36@gmail.com>
2026-02-24 18:38:20 +0530

    Program 26
    26. Replace a special string from a given paragraph with another string in Python.

---
+++ String_List/String-Programs/Program 26.py
@@ -0,0 +1,7 @@
+#26. Replace special string in paragraph
+
+para = input("Enter paragraph: ")
+old = input("Enter word to replace: ")
+new = input("Enter new word: ")
+
+print(para.replace(old, new))
