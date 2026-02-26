9d847c14d4be0471a4024ce5a65931d81a8895f3
Pratiksha Choudhary <choudharypratiksha36@gmail.com>
2026-02-27 00:02:37 +0530

    Program 17
    17. Write a Python program to create a tuple with different data types.

---
+++ Tuple_Dictionary/Tuple-Programs/Program 17.py
@@ -0,0 +1,8 @@
+#17. Tuple with different data types
+
+a = int(input("Enter integer: "))
+b = input("Enter string: ")
+c = float(input("Enter float: "))
+d = bool(input("Enter True/False: "))
+t = (a, b, c, d)
+print(t)
