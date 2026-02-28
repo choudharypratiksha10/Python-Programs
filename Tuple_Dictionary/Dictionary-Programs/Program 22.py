b8af571b04857c5a5c806a3f3a8f474914f61e5e
Pratiksha Choudhary <choudharypratiksha36@gmail.com>
2026-02-28 09:24:27 +0530

    Program 22
    22. Write a Python program to combine values in a list of dictionaries. 
    Sample data: 
    [{'item': 'item1', 'amount': 400}, {'item': 'item2', 'amount': 300}, {'item': 'item1’, 'amount': 750}] 
    Expected Output: 
    Counter ({'item1': 1150, 'item2': 300})

---
+++ Tuple_Dictionary/Dictionary-Programs/Program 22.py
@@ -0,0 +1,16 @@
+#22. Combine values in list of dictionaries
+
+from collections import Counter
+
+data = [
+    {'item': 'item1', 'amount': 400},
+    {'item': 'item2', 'amount': 300},
+    {'item': 'item1', 'amount': 750}
+]
+
+result = Counter()
+
+for d in data:
+    result[d['item']] += d['amount']
+
+print(result)
