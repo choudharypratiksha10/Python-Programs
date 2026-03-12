7272d4cdf6387f9eeb8552357476dc845de84bf5
Pratiksha Choudhary <choudharypratiksha36@gmail.com>
2026-03-12 22:10:41 +0530

    Program 20
    20. Write a Python program to detect the number of local variables declared in a function.
    Sample Output:
    3

---
+++ Function/Program 20.py
@@ -0,0 +1,9 @@
+# Program 20: Detect Number of Local Variables
+
+def local_variables():
+    a = 10
+    b = 20
+    c = 30
+    print("Number of local variables:", len(locals()))
+
+local_variables()
