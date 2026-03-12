6cb2d4a5829c5f25caee98de2736efe973f0c4af
Pratiksha Choudhary <choudharypratiksha36@gmail.com>
2026-03-12 22:07:36 +0530

    Program 17
    17. Write a Python program to create a chain of function decorators (bold, italic, underline etc.).

---
+++ Function/Program 17.py
@@ -0,0 +1,18 @@
+# Program 17: Chain of Decorators
+
+def bold(func):
+    def wrapper():
+        return "<b>" + func() + "</b>"
+    return wrapper
+
+def italic(func):
+    def wrapper():
+        return "<i>" + func() + "</i>"
+    return wrapper
+
+@bold
+@italic
+def message():
+    return "Hello"
+
+print(message())
