8fff7c62c976a2b8704833d69057a7216fa8577b
Pratiksha Choudhary <choudharypratiksha36@gmail.com>
2026-03-12 22:09:55 +0530

    Delete Function/Program 20.py

--- Function/Program 20.py
+++
@@ -1,9 +0,0 @@
-# Program 20: Detect Number of Local Variables
-
-def local_variables():
-    a = 10
-    b = 20
-    c = 30
-    print("Number of local variables:", len(locals()))
-
-local_variables()
